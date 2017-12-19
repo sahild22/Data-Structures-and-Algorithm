c7fa955c5133f6a45938c531c6c7b095022feddd
Sahil Diwan <sahildiwan22@gmail.com>
2017-12-19 17:32:56 -0500

    Two sum modified

--- TwoSum.java
+++ TwoSum.java
@@ -8,16 +8,18 @@
 
 class TwoSum{
 	public static int[] twoSum(int[] nums, int target){
-		for(int i = 0; i < nums.length - 1; i++){
-			for(int j = 1; j < nums.length; j++){
-				if(nums[i] + nums[j] == target){
-					if(i != j){
-						return new int[]{i, j};
-					}
-				}
+		if(nums == null || nums.length < 2){
+			return new int[]{0,0};
+		}
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i = 0; i < nums.length; i++){
+			if(map.containsKey(nums[i])){
+				return new int[]{map.get(nums[i]), i};
+			}else{
+				map.put(target-nums[i], i);
 			}
 		}
-		return null;
+		return new int[]{0,0};
 	}
 
 	public static void main(String[] args) {
